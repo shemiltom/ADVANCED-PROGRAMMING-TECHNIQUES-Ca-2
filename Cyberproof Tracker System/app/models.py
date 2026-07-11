ecd62e5881b174c21e76953f19fdf46d5ba54b7e
shemiltom <55620329+shemiltom@users.noreply.github.com>
2026-07-11 11:35:19 +0100

    db upate

--- Cyberproof Tracker System/app/models.py
+++ Cyberproof Tracker System/app/models.py
@@ -6,13 +6,13 @@ db = SQLAlchemy()
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    # Relationship to issues
+    description = db.Column(db.Text, nullable=False)
     issues = db.relationship('Issue', backref='project', lazy=True)
 
 class Issue(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=False)
-    severity = db.Column(db.String(20), nullable=False) # e.g., 'Critical', 'Low'
+    priority = db.Column(db.String(20), nullable=False) 
     status = db.Column(db.String(20), default='Open')
     project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
